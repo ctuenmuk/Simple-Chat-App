f29cad127f2259dfb05e035c859f42cb59013783
Coco <colintm@HyunKeyo.local>
2020-12-14 20:30:41 -0500

    update v1.0

--- intro_bot.py
+++
@@ -1,5 +0,0 @@
-greet_words=["hello", "hi","greetings","Yo","what's up"]
-greet_response = ["how can I help", "How are you today", "What are you feeling"]
-
-def check_greeting(sentence):
-    for word in sentence. 
